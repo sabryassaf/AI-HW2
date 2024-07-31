5eaccec2947632ad91b46c8d440bdc5f79776fbf
sabryassaf <sabry.assaf12@gmail.com>
2024-07-31 23:35:39 +0300

    expectimax working, final code

--- submission.py
+++ submission.py
@@ -71,8 +71,7 @@ class AgentMinimax(Agent):
 
     def min_max(self, env: WarehouseEnv, robot_id, depth):
         # check if the game is over, if we are close the time limit, or if the robot has no battery
-        if env.done() or env.num_steps <= 0 or env.get_robot(
-                robot_id).battery <= 0 or self.check_time_limit() or depth == 0:
+        if env.done() or env.num_steps <= 0 or env.get_robot(robot_id).battery <= 0 or self.check_time_limit() or depth == 0:
             return smart_heuristic(env, robot_id)
 
         # get the legal operators for the robot
@@ -141,8 +140,7 @@ class AgentAlphaBeta(Agent):
 
     def min_max(self, env: WarehouseEnv, robot_id, depth, alpha, beta):
         # check if the game is over, if we are close the time limit, or if the robot has no battery
-        if env.done() or env.num_steps <= 0 or env.get_robot(
-                robot_id).battery <= 0 or self.check_time_limit() or depth == 0:
+        if env.done() or env.num_steps <= 0 or env.get_robot(robot_id).battery <= 0 or self.check_time_limit() or depth == 0:
             return smart_heuristic(env, robot_id)
 
         # get the legal operators for the robot
@@ -213,64 +211,87 @@ class AgentExpectimax(Agent):
         self.start_time = None
         self.time_limit = None
 
-    def utility(self, env: WarehouseEnv, robot_id):
-        # Return the difference in credits between the two robots
-        return env.get_robot(robot_id).credit - env.get_robot((robot_id + 1) % 2).credit
-
     def check_time_limit(self):
-        # Check if the time limit has been reached
-        if (time.time() - self.start_time) >= self.time_limit:
-            raise FunctionTimedOut
+        if (time.time() - self.start_time) >= (self.time_limit - 0.2):
+            return True
+        return False
 
-    def AgentExpectimax(self, env: WarehouseEnv, robot_id):
-        if env.done():
-            return self.utility(env, robot_id)
-        return smart_heuristic(env, robot_id)
+    def expectimax(self, env: WarehouseEnv, robot_id, depth):
+        if env.done() or env.num_steps <= 0 or env.get_robot(robot_id).battery <= 0 or self.check_time_limit() or depth == 0:
+            return smart_heuristic(env, robot_id)
 
-    def expectimax(self, env: WarehouseEnv, robot_id):
-        if env.done() or (time.time() - self.start_time >= self.time_limit):
-            return self.AgentExpectimax(env, robot_id)
         operators = env.get_legal_operators(robot_id)
         children = [env.clone() for _ in operators]
+
         if robot_id == 0:
-            value = float('-inf')
-            for op in env.get_legal_operators(robot_id):
-                self.check_time_limit()
-                child = env.clone()
+            # this is our player we are maximizing it
+            cur_max = float('-inf')
+            for child, op in zip(children, operators):
                 child.apply_operator(robot_id, op)
-                value = max(value, self.expectimax(child, 1))
-            return value
+                v = self.expectimax(child, 1, depth - 1)
+                cur_max = max(cur_max, v)
+            return cur_max
         else:
+            # enemy player has a probability distribution over its moves
             total_probability = 0
             expected_value = 0
-            moves = self.env.get_legal_operators(robot_id)
             move_probabilities = []
 
             # initiate the probabilities for each move
-            for move in moves:
+            for move in operators:
+                # give east, pick up package a higher probability
                 if move in ["move east", "pick up package"]:
                     probability = 2
                 else:
                     probability = 1
+
+                #append the probability to the list
                 move_probabilities.append(probability)
                 total_probability += probability
 
-            for move, probability in zip(moves, move_probabilities):
-                child = env.clone()
+            # iterate over the moves and calculate the expected value, and normalize it
+            for move, probability, child in zip(operators, move_probabilities, children):
                 child.apply_operator(robot_id, move)
-                evaluation, _ = self.expectimax(child, True)
+
+                # call the expectimax recursively
+                evaluation = self.expectimax(child, 0, depth - 1)
                 expected_value += (probability / total_probability) * evaluation
         return expected_value
 
     def run_step(self, env: WarehouseEnv, agent_id, time_limit):
-        self.start_time = time.time()
+
+        # set the time limit for step
         self.time_limit = time_limit
-        operators = env.get_legal_operators(agent_id)
-        children = [env.clone() for _ in operators]
-        values = [self.expectimax(child, (agent_id + 1) % 2) for child in children]
-        max_value = max(values)
-        index_selected = values.index(max_value)
-        return operators[index_selected]
+        self.start_time = time.time()
+
+        # set depth
+        depth = 3
+
+        # default value for step
+        best_step_move = "move north"
+
+        def best_step():
+            nonlocal best_step_move
+            # get the legal operators for the robot
+            operators = env.get_legal_operators(agent_id)
+            children = [env.clone() for _ in operators]
+
+            for child, op in zip(children, operators):
+                child.apply_operator(agent_id, op)
+
+            # get the value of each child
+            values = [self.expectimax(child, 1 - agent_id, depth) for child in children]
+            # get the index of the child with the maximum value
+            max_value = max(values)
+            index_selected = values.index(max_value)
+            best_step_move = operators[index_selected]
+
+        try:
+            func_timeout(time_limit - 0.1, best_step)
+        except FunctionTimedOut:
+            pass
+
+        return best_step_move
 
 
 # here you can check specific paths to get to know the environment
